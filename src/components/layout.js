d9297c5e844ff95c1df3cc2f1000b25e46ae3456
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2020-05-08 08:28:54 -0600

    Readd dark mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,15 +2,15 @@ import React from "react"
 import PropTypes from "prop-types"
 import Styled, { ThemeProvider } from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
-// import { lightTheme, darkTheme } from "../themes/theme"
-// import { GlobalStyles } from "../themes/global"
-// import { useDarkMode } from "./useDarkMode"
+import { lightTheme, darkTheme } from "../themes/theme"
+import { GlobalStyles } from "../themes/global"
+import { useDarkMode } from "./useDarkMode"
 import { Container } from "./containers"
 import "./navigation/nav.css"
 import { MotionNav } from "./navigation/MotionNav"
 import Header from "./header"
 import "./layout.css"
-// import Toggle from "./toggle"
+import Toggle from "./toggle"
 
 const Footer = Styled.footer`
   background: #000;
@@ -32,18 +32,25 @@ const Layout = ({ children }) => {
     }
   `)
 
-  // const [theme, toggleTheme, componentMounted] = useDarkMode()
-  // const themeMode = theme === "light" ? lightTheme : darkTheme
+  const [theme, toggleTheme, componentMounted] = useDarkMode()
+  const themeMode = theme === "light" ? lightTheme : darkTheme
 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <MotionNav />
-
-      <div className="content">
-        <Container>{children}</Container>
-      </div>
-
+      {!componentMounted && (
+        <div style={{ height: "100vh", background: "#111" }} />
+      )}
+      {componentMounted && (
+        <div className="content">
+          <ThemeProvider theme={themeMode}>
+            <GlobalStyles />
+            <Toggle theme={theme} toggleTheme={toggleTheme} />
+            <Container>{children}</Container>
+          </ThemeProvider>
+        </div>
+      )}
       <Footer>P.O. Box 1726 | Bolingbrook | IL | 60440 | 630-759-5033</Footer>
     </>
   )
